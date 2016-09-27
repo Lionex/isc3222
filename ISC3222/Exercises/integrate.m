eb2595aa5c3ef7669655130d0188767efffd5eda
Gwen Lofman <Gwen@Lofman.co>
2016-09-27 11:10:54 -0400

    Adds explanatory comments

---
+++ ISC3222/Exercises/integrate.m
@@ -0,0 +1,33 @@
+function s = trapezoids( f, a, b, n )
+% TRAPEZOIDS calculates the numerical approximation of a definite integral
+%   Uses the trapezoidal rule to calculate the sum of the areas of
+%   trapezoids that approximate the curve.  Precision increases with larger
+%   n, which represents the number of trapezoids.
+%
+% Synopsis: TRAPEZOIDS(f)
+%           TRAPEZOIDS(f, b)
+%           TRAPEZOIDS(f, a, b)
+%           TRAPEZOIDS(f, a, b, n)
+%
+% Output:   s = TRAPEZOIDS(f, a, b, n)
+
+% defines the default values of optional arguments
+defaultStart = 0;
+defaultEnd = 1;
+defaultSteps = 25;
+
+switch nargin
+    case 1
+        a = defaultStart;
+        b = defaultEnd;
+        n = defaultSteps;
+    case 2 % sets value of a and b so TRAPEZOID(f, b) evaluates from 0 to b
+        b = a;
+        a = defaultStart;
+        n = defaultSteps;
+    case 3
+        n = defaultSteps;
+end
+
+end
+
